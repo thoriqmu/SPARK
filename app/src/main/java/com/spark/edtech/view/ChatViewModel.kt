3d24eb73a7eb50f129530e0f425a8cdb31130405
thoriqmu <tmuchlishin@gmail.com>
2025-02-20 15:17:57 +0700

    Initial commit

---
+++ app/src/main/java/com/spark/edtech/view/ChatViewModel.kt
@@ -0,0 +1,7 @@
+package com.spark.edtech.view
+
+import androidx.lifecycle.ViewModel
+
+class ChatViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/spark/edtech/view/ProfileViewModel.kt
@@ -0,0 +1,7 @@
+package com.spark.edtech.view
+
+import androidx.lifecycle.ViewModel
+
+class ProfileViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
