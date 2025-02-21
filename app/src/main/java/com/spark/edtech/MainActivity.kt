59651214ca3abd44112a15cb2b44fa065f7432b9
thoriqmu <tmuchlishin@gmail.com>
2025-02-21 10:21:10 +0700

    Enable view binding, set up bottom navigation, and add fragments

--- app/src/main/java/com/spark/edtech/MainActivity.kt
+++ app/src/main/java/com/spark/edtech/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.spark.edtech
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.spark.edtech.databinding.ActivityMainBinding
+import com.spark.edtech.view.ChatFragment
+import com.spark.edtech.view.HomeFragment
+import com.spark.edtech.view.NotificationFragment
+import com.spark.edtech.view.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            fun loadFragment(fragment: Fragment) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.nav_host_fragment, fragment)
+                    .commit()
+            }
+
+            when (item.itemId) {
+                R.id.homeFragment -> loadFragment(HomeFragment())
+                R.id.chatFragment -> loadFragment(ChatFragment())
+                R.id.notificationFragment -> loadFragment(NotificationFragment())
+                R.id.profileFragment -> loadFragment(ProfileFragment())
+                else -> throw IllegalArgumentException("Invalid item ID")
+            }
+            true
         }
+
     }
 }
