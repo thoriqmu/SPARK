5d82a71153cbbeaf72270a15e7f54a3417c65ff6
Thoriq Muchlishin <tmuchlishin@gmail.com>
2025-06-02 11:01:53 +0700

    Create Reedem UI and Refactor layout components
    - Create Reedem Code activity and its corresponding layout.
    - Adjust UI components in Login, Register, Chat, and Main activities.
    - Integrate Lottie library for animations.
    - Update `AndroidManifest.xml` to include the new ReedemActivity.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,5 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
     implementation(libs.circleprogress)
+    implementation(libs.lottie)
 }
---
+++ app/src/main/java/com/spark/edtech/ui/view/auth/ReedemActivity.kt
@@ -0,0 +1,21 @@
+package com.spark.edtech.ui.view.auth
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.spark.edtech.R
+
+class ReedemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_reedem)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
